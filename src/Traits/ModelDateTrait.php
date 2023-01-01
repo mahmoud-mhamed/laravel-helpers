065ce5d6b23a43f9cabb1d9eb67217357dc5a9c4
mahmoud-mhamed <mahmoud-mhamed@users.noreply.github.com>
2023-01-01 07:31:34 +0000

    Fix styling

--- src/Traits/ModelDateTrait.php
+++ src/Traits/ModelDateTrait.php
@@ -9,17 +9,20 @@ trait ModelDateTrait
     //for update date created_at and updated_at format
     public function getCreatedAtAttribute($date): string
     {
-        if ($date!=null)
+        if ($date != null) {
             return Carbon::parse($date)->format('Y-m-d');
+        }
+
         return Carbon::now()->format('Y-m-d');
-        if ($date!==null) {
+        if ($date !== null) {
             $carbon = Carbon::parse($date);
             if ($carbon->diffInHours() < 24) {
                 return $carbon->diffForHumans();
             }
-            if($carbon->diffInDays() < 7) {
+            if ($carbon->diffInDays() < 7) {
                 return $carbon->format('Y-m-d h:i A');
             }
+
             return $carbon->format('Y-m-d');
         }
 
@@ -28,8 +31,10 @@ trait ModelDateTrait
 
     public function getUpdatedAtAttribute($date)
     {
-        if ($date!=null)
+        if ($date != null) {
             return Carbon::parse($date)->format('Y-m-d h:i A');
+        }
+
         return $date;
     }
 }
