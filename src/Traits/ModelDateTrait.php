18b8150898c9f4e077ee9b8c962ff2b21c92df77
mahmoud-mhamed <mahmoud-mhamed@users.noreply.github.com>
2023-01-02 21:17:05 +0000

    Fix styling

--- src/Traits/ModelDateTrait.php
+++ src/Traits/ModelDateTrait.php
@@ -3,7 +3,6 @@
 namespace Mahmoudmhamed\LaravelHelpers\Traits;
 
 use Carbon\Carbon;
-use Spatie\LaravelIgnition\Tests\Support\Models\Car;
 
 trait model_date_trait
 {
@@ -20,19 +19,20 @@ trait model_date_trait
 
     private function getDate($date): ?string
     {
-        if (!$date){
+        if (! $date) {
             return config('helpers.model_date_trait.null_value');
         }
         $carbon = Carbon::parse($date);
-        if (config('helpers.model_date_trait.format_diff_for_human_when_less_than_or_equal_hour')!==null &&
+        if (config('helpers.model_date_trait.format_diff_for_human_when_less_than_or_equal_hour') !== null &&
             $carbon->diffInHours() < config('helpers.model_date_trait.format_diff_for_human_when_less_than_or_equal_hour')) {
             return $carbon->diffForHumans();
         }
 
-        if (config('helpers.model_date_trait.format_diff_in_day_grater_than.value')!==null &&
+        if (config('helpers.model_date_trait.format_diff_in_day_grater_than.value') !== null &&
             $carbon->diffInDays() < config('helpers.model_date_trait.format_diff_in_day_grater_than.value')) {
             return $carbon->format(config('helpers.model_date_trait.format_diff_in_day_grater_than.format'));
         }
+
         return $carbon->format(config('helpers.model_date_trait.format'));
     }
 }
