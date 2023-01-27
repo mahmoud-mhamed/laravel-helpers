4971c2d86d0266d06be8422fe87a27587aa4ef08
mahmoud-mhamed <mahmoud1272022@gmail.com>
2023-01-27 17:33:16 +0300

    [FEAT] Add some functions to enum options trait

--- src/Traits/EnumOptionsTrait.php
+++ src/Traits/EnumOptionsTrait.php
@@ -14,8 +14,28 @@ trait EnumOptionsTrait
     {
         return collect(self::cases())->map(function ($row){
             $item['id'] = $row;
-            $item['name'] = __(config('helpers.enum_options_trait.trans_file_name').'.' . class_basename(__CLASS__) . '.' . $row->value);
+            $item['name'] = self::getTrans($row);
             return $item;
         });
+    }
+
+    public static function getTrans($case=null):?string
+    {
+        return $case?__(config('helpers.enum_options_trait.trans_file_name','enums').'.' . class_basename(__CLASS__) . '.' . ($case?->value??$case)):null;
+    }
+
+    public static function names(): array
+    {
+        return array_column(self::cases(), 'name');
+    }
+
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
+
+    public static function array(): array
+    {
+        return array_combine(self::values(), self::names());
     }
 }
